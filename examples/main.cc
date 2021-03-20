e8389bf2aefa1869ca78e4bd16184e0afa6cb33b
zbb775885@163.com <zbb775885@163.com>
2021-03-20 11:52:53 +0800

    add abs factory success

--- examples/main.cc
+++ examples/main.cc
@@ -4,7 +4,7 @@
  * @Author: 周波
  * @Date: 2021-03-20 08:58:42
  * @LastEditors: 周波
- * @LastEditTime: 2021-03-20 11:46:03
+ * @LastEditTime: 2021-03-20 11:52:45
  * @FilePath: \lemon\examples\main.cc
  */
 #include <lemon_abs_factory.hh>
@@ -34,7 +34,7 @@ int32_t main()
     Base *base = lemon::AbsFactory<Base>::Create("aaa");
     Derive *derive = static_cast<Derive *>(base);
     std::cout << typeid(base).name() << std::endl;
-    std::cout << derive->a_ << " ddd  " << std::endl;
+    std::cout << derive->a_ << " ddda  " << std::endl;
 
     lemon::AbsFactory<Base, int32_t>::Register<Derive>(1);
     Base *base1 = lemon::AbsFactory<Base, int32_t>::Create(1);
